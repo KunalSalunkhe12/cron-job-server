9dc17222b7f06c26e66d4d64318288364fc258ef
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-07-11 22:25:23 +0530

    chore: Add initial project files and dependencies

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const axios = require("axios");
+const cron = require("node-cron");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const accredianServer = "https://accredian-backend-task-iohc.onrender.com";
+
+const getReferralsAccredian = async () => {
+  try {
+    const response = await axios.get(accredianServer + "/referrals");
+    console.log(`Ping successful: ${response}`);
+  } catch (error) {
+    console.error(`Error pinging server: ${error.message}`);
+  }
+};
+
+cron.schedule("*/15 * * * *", getReferralsAccredian);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on port, " + PORT);
+});
